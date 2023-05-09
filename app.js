9d03cfa6bfadd52afedfa0b50529c0e367f170f7
Nehathvarma1616 <nehathvarma@gmail.com>
2023-05-09 22:36:55 +0530

    admin,player pages created and db created

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.set('view engine', 'ejs');
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-
-	// The render method takes the name of the HTML
-	// page to be rendered as input.
-	// This page should be in views folder in
-	// the root directory.
-	// We can pass multiple properties and values
-	// as an object, here we are passing the only name
-	res.render('home', { name: 'Nehath' });
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
+app.get("/admin_index", (request, response) => {
+  response.render("admin_index", );
+});
+app.get("/player_index", (request, response) => {
+	response.render("player_index", );
+  });
+app.get("/admin_singnup", (request, response) => {
+	response.render("admin_singnup", );
+  });
+
+app.get("/player_signup", (request, response) => {
+	response.render("player_signup", );
+});
 
 module.exports = app;
